1bbe22d4c68ba69ae469f4ca47131b16b27e9f42
Tadeas Drab <tadeas.drab@student.tuke.sk>
2024-04-07 00:49:08 +0200

    Fixed cache

--- src/pages/api/stock/names.ts
+++ src/pages/api/stock/names.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiResponse } from "next";
 import { cacheProvider, cacheProviderGet, cacheProviderHas } from '@/lib/cache/cacheProvider';
 import { stockNames } from '@/lib/static/stockNames';
 import { InterestsEnumArray, SuccessType } from '@/types';
@@ -27,9 +27,7 @@ export default async function handler(
         case "GET":
             return await GET(req, res);
         default:
-            return res
-                .status(400)
-                .json({ success: false, error: `You are not allowed to access this site!` });
+            return res.status(400).json({ success: false, error: `You are not allowed to access this site!` });
     }
 }
 
@@ -39,28 +37,36 @@ async function GET(
 ) {
     const symbols = InterestsEnumArray.flatMap(interest => stockNames(interest));
     const token = 'co8l9qhr01qjn9a1tfbgco8l9qhr01qjn9a1tfc0';
-    const promises = symbols.filter(symbol => !cacheProviderHas(symbol + "-NAMES")).map(symbol => {
-        const url = `https://finnhub.io/api/v1/search?q=${symbol}&token=${token}`;
-        return fetch(url).then(response => response.json()).catch(error => console.error('Error:', error));
-    });
-
-    let data = await Promise.all(promises);
+    let dataArray: [string, any][] = [];
+    const filteredSymbols = symbols.filter(symbol => !cacheProviderHas(symbol + "-NAMES"))
+    for (let i = 0; i < filteredSymbols.length; i++) {
+        try {
+            const symbol = filteredSymbols[i];
+            const url = `https://finnhub.io/api/v1/search?q=${symbol}&token=${token}`;
+            const fetchReq = await fetch(url);
+            if (fetchReq.status < 200 || fetchReq.status >= 300) {
+                console.log("Error fetching data", "status:", fetchReq.status, "statusText:", fetchReq.statusText)
+                continue;
+            }
+            let data = await fetchReq.json();
+            data.result = data.result.slice(0, 1);
+            dataArray.push([symbol, data]);
+            console.log("Fetched data for", symbol);
+        } catch (error) {
+            console.log("Error fetching data", error);
+        }
+    }
 
     const cachedData = symbols.filter(symbol => cacheProviderHas(symbol + "-NAMES")).map(symbol => cacheProviderGet(symbol + "-NAMES"));
 
-    data = data.concat(cachedData);
+    dataArray = dataArray.concat(cachedData);
 
-    const toReturn = data.map((item: any, index: number) => {
-        if (!item || item.error || item.result[0].error) {
-            return ({
-                symbol: "Error fetching data",
-                name: 'Error fetching data',
-            })
-        }
-        cacheProvider(item.result[0].symbol + "-NAMES", item, Date.now() + 1000 * 60 * 60 * 24);
+    const toReturn = dataArray.map((item: any, index: number) => {
+        console.log(item);
+        cacheProvider(item[0] + "-NAMES", item, Date.now() + 1000 * 60 * 60 * 24);
         return ({
-            symbol: item.result[0].symbol,
-            name: item.result[0].description,
+            symbol: item[0],
+            name: item[1].result[0].description,
         })
     });
 
