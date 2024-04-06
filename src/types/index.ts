fe1b71e9c86267af0c4ecfb45ef8500714565686
Vavro <vavro.murcko@gmail.com>
2024-04-06 20:56:34 +0200

    Form completed

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,22 @@ export const InterestsEnum = {
 } as const;
 
 export type InterestsEnum = keyof typeof InterestsEnum;
+
+export const RiskToleranceEnum = {
+  LOW: "LOW",
+  MEDIUM: "MEDIUM",
+  HIGH: "HIGH",
+} as const;
+
+export type RiskToleranceEnum = keyof typeof RiskToleranceEnum;
+
+export const IntervalEnum = {
+  UNCERTAIN: "UNCERTAIN",
+  "<1": "<1",
+  "1_3": "1_3",
+  "3_5": "3_5",
+  "5_10": "5_10",
+  "10+": "10+",
+} as const;
+
+export type IntervalEnum = keyof typeof IntervalEnum;
