23c51bda0a1a313fff2e0f11d9360d2a65153d25
Samuel Neceda <69465264+SamuelNeceda@users.noreply.github.com>
2024-04-07 08:39:50 +0200

    load data from formular

--- src/pages/logic.tsx
+++ src/pages/logic.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
-import {
-  getStocks,
-} from "@/lib/static/stockNames";
+import { getStocks } from "@/lib/static/stockNames";
+import { usePreferencesState } from "@/state/preferencesState";
 
 import {
   Table,
@@ -11,17 +10,20 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 
 export default function LogicStocks() {
   const [data, setData] = useState<Record<string, any>>([]);
 
   useEffect(() => {
     (async () => {
-      const stocks = await getStocks(["TECH"], 900);
-      console.log(stocks)
+      const stocks = await getStocks(
+        usePreferencesState.getState().interests,
+        usePreferencesState.getState().budget ?? 0
+      );
+      console.log(stocks);
 
-      setData(stocks)
+      setData(stocks);
     })();
   }, []);
 
@@ -47,33 +49,33 @@ export default function LogicStocks() {
     }
   }
 
-  return <>
-    <Table>
-      <TableCaption>A list of your owned Stocks.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-max">Name</TableHead>
-          <TableHead>StockPrice</TableHead>
-          <TableHead>Amount</TableHead>
-          <TableHead>History</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {Object.entries(data).map((value: [string, any]) => (
-          <TableRow key={value[0]}>
-            <TableCell className="font-medium">{value[1].name}</TableCell>
-            <TableCell>{value[1].stockPrice}</TableCell>
-            <TableCell>{handleInfinity(roundToFourDecimalPlaces(value[1].amount))}</TableCell>
-            <TableCell>{
-              <img
-                src={getRandomImage()}
-                className="ml-4"
-              />
-            }</TableCell>
+  return (
+    <>
+      <Table>
+        <TableCaption>A list of your owned Stocks.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-max">Name</TableHead>
+            <TableHead>StockPrice</TableHead>
+            <TableHead>Amount</TableHead>
+            <TableHead>History</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-
-  </>;
+        </TableHeader>
+        <TableBody>
+          {Object.entries(data).map((value: [string, any]) => (
+            <TableRow key={value[0]}>
+              <TableCell className="font-medium">{value[1].name}</TableCell>
+              <TableCell>{value[1].stockPrice}</TableCell>
+              <TableCell>
+                {handleInfinity(roundToFourDecimalPlaces(value[1].amount))}
+              </TableCell>
+              <TableCell>
+                {<img src={getRandomImage()} className="ml-4" />}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
 }
